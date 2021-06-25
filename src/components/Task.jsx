27543fb444dedacfd009e1496c9c4842a2db56ff
Maikol Hernandez <maikol_hernandez_rm@hotmail.com>
2021-06-25 13:34:48 -0400

    Components - Task - setting basic state to handle task remove

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Task = ({ task }) => {
+  const [disabled, setDisabled] = useState(false)
+  const handleRemove = (id) => {
+    try {
+      alert(id)
+    } catch (e) { console.log(e) }
+  }
   return(
     <div className={"task " + (task.completed ? 'completed' : '')}>
       <button className={"task__status fas " + (task.completed ? 'fa-check' : 'fa-clock')}></button>
@@ -9,7 +15,7 @@ export const Task = ({ task }) => {
         { task.description }
         <div className="task__actions">
           <button className="task__action fas fa-cog"></button>
-          <button className="task__action task__action--alert fas fa-trash"></button>
+          <button disabled={disabled} className={"task__action task__action--alert fas " + (disabled ? 'fa-circle-notch' : 'fa-trash')} onClick={() => handleRemove(task.id)}></button>
         </div>
       </div>
     </div>
