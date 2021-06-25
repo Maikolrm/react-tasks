11aa3e4e2491938f214178157f722f85d5e1f944
Maikol Hernandez <maikol_hernandez_rm@hotmail.com>
2021-06-25 12:49:16 -0400

    Components - Dashboard - import and use CSSTransition to animate form

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,12 +1,15 @@
 import React, { useContext } from 'react'
 import { TopSection, Content, Navigation, CreateTask } from './'
+import { CSSTransition } from 'react-transition-group'
 import StateContext from '../StateContext'
 
 export const Dashboard = () => {
   const { isVisible } = useContext(StateContext)
   return(
     <>
-      { isVisible ? <CreateTask /> : '' }
+      <CSSTransition in={isVisible} timeout={200} classNames='modal' unmountOnExit>
+        <CreateTask />
+      </CSSTransition>
       <TopSection />
       <Content />
       <Navigation />
