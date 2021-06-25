3359bbbc6d1f5c772d318f3f94ca800fb6f028f3
Maikol Hernandez <maikol_hernandez_rm@hotmail.com>
2021-06-25 12:00:08 -0400

    Project - App - creating app initial state using immer

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,51 @@
 import React from 'react'
+import { useImmerReducer } from 'use-immer'
 
 // STYLES
 import './css/reset.css'
 import './css/modal.css'
+import './css/loader.css'
 import './css/form.css'
 import './css/top-section.css'
 import './css/content.css'
 import './css/tasks.css'
 import './css/nav.css'
+import './css/media.css'
 
 // COMPONENTS
 import { Homepage, Dashboard } from './components'
 
+// CONTEXT
+import StateContext from './StateContext'
+import DispatchContext from './DispatchContext'
+
 export const App = () => {
-  const loggedIn = false
+  const initialState = {
+    tasks: [],
+    user: '',
+    loggedIn: false,
+    isVisible: false
+  }
+  const reducer = (draft, action) => {
+    switch (action.type) {
+      case 'login':
+        draft.loggedIn = true
+        break
+      case 'logout':
+        draft.loggedIn = false
+        break
+      case 'show-form':
+        draft.isVisible = action.value
+        break
+    }
+  }
+  const [state, dispatch] = useImmerReducer(reducer, initialState)
   return(
-    <>
-      { loggedIn ? <Dashboard /> : <Homepage /> }
-    </>
+    <StateContext.Provider value={state}>
+      <DispatchContext.Provider value={dispatch}>
+        { state.loggedIn ? <Dashboard /> : <Homepage /> }
+      </DispatchContext.Provider>
+    </StateContext.Provider>
   )
 }
 
