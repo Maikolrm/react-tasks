afe60d7da3211bb0a37f3857502263213157de2b
Maikol Hernandez <maikol_hernandez_rm@hotmail.com>
2021-06-25 13:24:48 -0400

    Components - Content - pulling tasks from state and map through them

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,13 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Task } from './'
+import StateContext from '../StateContext'
 
 export const Content = () => {
-  const tasks = [
-    {id: Math.random(), description: 'Test one', completed: 0, createdDate: '9:01' },
-    {id: Math.random(), description: 'Test two', completed: 0, createdDate: '9:02' },
-    {id: Math.random(), description: 'Test three', completed: 0, createdDate: '9:03' },
-    {id: Math.random(), description: 'Test four', completed: 0, createdDate: '9:04' }
-  ]
+  const { tasks } = useContext(StateContext)
   return(
     <div className="content content--dashboard">
       <h2 className="content__heading">Tasks</h2>
