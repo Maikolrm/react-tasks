a98de1ff8c33539d44c08b5f8df4aa0c4f06520f
Maikol Hernandez <maikol_hernandez_rm@hotmail.com>
2021-06-25 13:10:56 -0400

    Components - TopSection - pulling user information from app state

--- src/components/TopSection.jsx
+++ src/components/TopSection.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import avatar from '../yo.png'
+import React, { useContext } from 'react'
+import StateContext from '../StateContext'
+
 export const TopSection = () => {
+  const { user, tasks } = useContext(StateContext)
   return(
     <div className="top-section">
       <div className="top-section__user">
         <div className="user__avatar">
-          <img src={avatar} alt='Maikol Hernandez' />
+          <img src={user.avatar} alt={`${user.name} ${user.lastname}`} />
         </div>
         <div className="user__info">
-          <h2 className="user__name">Maikol Hernandez</h2>
-          <p className="user__tasks">You have {5} tasks</p>
+          <h2 className="user__name">{user.name} {user.lastname}</h2>
+          <p className="user__tasks">You have {tasks.length} tasks</p>
         </div>
       </div>
     </div>
