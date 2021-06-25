cc6a4ee4d6f18e8bbb4923eea09c0aff8de8d4c2
Maikol Hernandez <maikol_hernandez_rm@hotmail.com>
2021-06-25 12:41:23 -0400

    Components - Navigation - dispatch events to logout and show create tasks form

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import DispatchContext from '../DispatchContext'
 
 export const Navigation = () => {
+  const dispatch = useContext(DispatchContext)
   return(
     <nav className="nav">
       <button className="nav__btn fas fa-tasks selected"></button>
-      <button className="nav__btn fas fa-plus"></button>
-      <button className="nav__btn fas fa-sign-out-alt"></button>
+      <button className="nav__btn fas fa-plus" onClick={() => dispatch({ type: 'show-form', value: true })}></button>
+      <button className="nav__btn fas fa-sign-out-alt" onClick={() => dispatch({ type: 'logout' })}></button>
     </nav>
   )
 }
