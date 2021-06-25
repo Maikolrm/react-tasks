9d96b45ba4de4e9988a5e8a278e6921e544afe51
Maikol Hernandez <maikol_hernandez_rm@hotmail.com>
2021-06-25 13:18:16 -0400

    Components - Dashboard - import fetchTasks from api and fetch user tasks

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,9 +2,19 @@ import React, { useContext } from 'react'
 import { Page, TopSection, Content, Navigation, CreateTask } from './'
 import { CSSTransition } from 'react-transition-group'
 import StateContext from '../StateContext'
+import { fetchTasks } from '../api'
 
 export const Dashboard = () => {
   const { isVisible } = useContext(StateContext)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const tasks = await fetchTasks()
+        console.log(tasks)
+      } catch (e) { console.log(e) }
+    }
+    fetchData()
+  }, [])
   return(
     <Page title='Dasbaord!' >
       <CSSTransition in={isVisible} timeout={200} classNames='modal' unmountOnExit>
