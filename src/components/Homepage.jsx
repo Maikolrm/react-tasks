7be14e35b63aff99db2fdc62731a4f5668177575
Maikol Hernandez <maikol_hernandez_rm@hotmail.com>
2021-06-25 12:37:51 -0400

    Components - Homepage - import and render Login component

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Login } from './'
 
 export const Homepage = () => {
   return(
@@ -6,25 +7,7 @@ export const Homepage = () => {
       <div className="homepage__top-section">
         <span className="top-section__icon fas fa-tasks"></span>
       </div>
-      <form autoComplete="off">
-        <h2 className="form__heading">Login</h2>
-        {/* CONTENT */}
-        <div className="form__content">
-          <div className="form__row">
-            <span className="field__icon fas fa-envelope"></span>
-            <input value="user@demo.com" className="form__field" name="email" readOnly />
-          </div>
-          <div className="form__row">
-          <span className="field__icon fas fa-lock"></span>
-            <input value="demo12345678" type='text' className="form__field" name="password" readOnly />
-            <span className="field__icon field__icon--action fas fa-eye"></span>
-          </div>
-          <button className="form__btn">Login</button>
-        </div>
-        {/* /CONTENT */}
-      </form>
+      <Login />
     </div>
   )
 }
-
-export default Homepage
