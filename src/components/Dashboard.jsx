04303280a50602e9ce8e0191395d89ca9f81a79e
Maikol Hernandez <maikol_hernandez_rm@hotmail.com>
2021-06-26 11:33:52 -0400

    Components - Dashboard - wrapping content into Page component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,18 +1,18 @@
 import React, { useContext } from 'react'
-import { TopSection, Content, Navigation, CreateTask } from './'
+import { Page, TopSection, Content, Navigation, CreateTask } from './'
 import { CSSTransition } from 'react-transition-group'
 import StateContext from '../StateContext'
 
 export const Dashboard = () => {
   const { isVisible } = useContext(StateContext)
   return(
-    <>
+    <Page title='Dasbaord!' >
       <CSSTransition in={isVisible} timeout={200} classNames='modal' unmountOnExit>
         <CreateTask />
       </CSSTransition>
       <TopSection />
       <Content />
       <Navigation />
-    </>
+    </Page>
   )
 }
